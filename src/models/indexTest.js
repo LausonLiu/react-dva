29225e1da0c494ce316ced62cc9cc68d5731e967
Lauson <1063681259@qq.com>
2019-10-07 16:54:12 +0800

    set proxy and async

---
+++ src/models/indexTest.js
@@ -0,0 +1,45 @@
+import * as apis from "../services/example"
+
+export default {
+    namespace: "indexTest",
+    state:{
+        name:"model模块使用",
+        cnodeData:[],
+    },
+
+    reducers:{
+        setName(state, payload){
+            // console.log(payload)
+            let _state = JSON.parse(JSON.stringify(state))
+            _state.name = payload.data.name
+            return _state;
+        },
+        setCnodeDataList(state, payload){
+            let _state = JSON.parse(JSON.stringify(state))
+            console.log(payload)
+            _state.cnodeData = payload.data
+            return _state;
+        }
+    },
+    effects:{
+        *setNameAsync({payload},{put,call}){
+            yield put({
+                type:"setName",
+                data:{
+                    name:"超人强"
+                }
+            })
+        },
+        *testCnode({payload},{put,call}){
+            let rel = yield call(apis.testCnode)
+            if (rel.data) {
+                // console.log(rel.data.data);
+                yield put({
+                    type:"setCnodeDataList",
+                    data:rel.data.data
+                })
+            }
+            
+        }
+    }
+}
---
+++ src/routes/IndexPage.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { connect } from 'dva';
+
+import * as apis from "../services/example"
+
+class IndexPage extends React.Component {
+
+  handleSetName = () => {
+    // console.log(this.props);
+    this.props.dispatch({
+      type:"indexTest/setName",
+      data:{
+        name:"猪猪侠"
+      }
+    })
+  }
+
+  handleSetNameAsync = () => {
+    this.props.dispatch({
+      type:"indexTest/setNameAsync",
+      data:{
+        name:"异步的猪猪侠"
+      }
+    })
+  }
+
+  componentDidMount(){
+    apis.testCnode().then((res) => {
+      console.log(res)
+    })
+  }
+
+  testCnode = () => {
+    this.props.dispatch({
+      type:"indexTest/testCnode"
+    })
+  }
+
+  render() {
+    console.log(this.props.cnodeData)
+    return (
+      <div>
+        我是首页组件
+        <p>{this.props.msg}</p>
+        <hr/>
+        <p>{this.props.name}</p>
+        <button onClick={ this.handleSetName } >setName</button>
+        <button onClick={ this.handleSetNameAsync } >setNameAsync</button>
+        <button onClick={ this.testCnode } >testCnode</button>
+      </div>
+    )
+  }
+}
+
+// IndexPage.propTypes = {
+// };
+const mapStateToProps = state => {
+  console.log(state);
+  
+  return {
+    msg:"我爱北京天安门",
+    name: state.indexTest.name,
+    cnodeData:state.indexTest.cnodeData
+  }
+}
+
+export default connect(mapStateToProps)(IndexPage);
