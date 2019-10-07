e30727914c23927a93e3b0311227a9c2361b4434
Lauson <1063681259@qq.com>
2019-10-07 15:19:52 +0800

    use dva-modles about redux

--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ import dva from 'dva';
 import './index.css';
 
 // 1. Initialize
-const app = dva();
-
+// const app = dva();
+import { createBrowserHistory as createHistory} from 'history';
+const app = dva({
+  history: createHistory(),
+});
 // 2. Plugins
 // app.use({});
 
 // 3. Model
 // app.model(require('./models/example').default);
+app.model(require('./models/indexTest').default);
 
 // 4. Router
 app.router(require('./router').default);
